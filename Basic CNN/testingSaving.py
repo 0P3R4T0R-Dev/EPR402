a48fbf56e4a3dc243eecb2e82b0a2e40bbd030f6
Stefan de Jager <stefan.dj1356@gmail.com>
2023-08-13 12:38:00 +0200

    why is this not working

--- Basic CNN/testingSaving.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from NN_components import *
-
-data = np.load("my_data.npz", allow_pickle=True)
-for name in data:
-    print("name:", name, "| array:", data[name])
-    hey = data[name].item()
-    print(hey.weights)
